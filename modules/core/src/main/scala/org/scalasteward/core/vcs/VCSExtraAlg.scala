9420dcfc8d1e6a737709b6eda797d312888cdb24
ekalashnikov <eugene.kalashnikov@agoda.com>
2020-09-25 13:39:48 +0700

    Migrate vcs package methods to VCSExtraAlg

--- modules/core/src/main/scala/org/scalasteward/core/vcs/VCSExtraAlg.scala
+++ modules/core/src/main/scala/org/scalasteward/core/vcs/VCSExtraAlg.scala
@@ -19,10 +19,11 @@ package org.scalasteward.core.vcs
 import cats.Monad
 import cats.syntax.all._
 import org.http4s.Uri
-import org.scalasteward.core.application.Config
+import org.scalasteward.core.application.SupportedVCS.{Bitbucket, BitbucketServer, GitHub, Gitlab}
+import org.scalasteward.core.application.{Config, SupportedVCS}
+import org.scalasteward.core.data.ReleaseRelatedUrl.VersionDiff
 import org.scalasteward.core.data.{ReleaseRelatedUrl, Update}
 import org.scalasteward.core.util.HttpExistenceClient
-import org.scalasteward.core.vcs
 
 trait VCSExtraAlg[F[_]] {
   def getReleaseRelatedUrls(repoUrl: Uri, update: Update): F[List[ReleaseRelatedUrl]]
@@ -36,8 +37,119 @@ object VCSExtraAlg {
   ): VCSExtraAlg[F] =
     new VCSExtraAlg[F] {
       override def getReleaseRelatedUrls(repoUrl: Uri, update: Update): F[List[ReleaseRelatedUrl]] =
-        vcs
-          .possibleReleaseRelatedUrls(config.vcsType, config.vcsApiHost, repoUrl, update)
+        possibleReleaseRelatedUrls(config.vcsType, config.vcsApiHost, repoUrl, update)
           .filterA(releaseRelatedUrl => existenceClient.exists(releaseRelatedUrl.url))
     }
+
+  private[vcs] def possibleReleaseRelatedUrls(
+      vcsType: SupportedVCS,
+      vcsUri: Uri,
+      repoUrl: Uri,
+      update: Update
+  ): List[ReleaseRelatedUrl] = {
+    val repoVCSType = extractRepoVCSType(vcsType, vcsUri, repoUrl)
+
+    val github = repoVCSType
+      .collect { case GitHub =>
+        possibleTags(update.nextVersion).map(tag =>
+          ReleaseRelatedUrl.GitHubReleaseNotes(repoUrl / "releases" / "tag" / tag)
+        )
+      }
+      .getOrElse(List.empty)
+
+    def files(fileNames: List[String]): List[Uri] = {
+      val maybeSegments = repoVCSType.map {
+        case SupportedVCS.GitHub | SupportedVCS.Gitlab             => List("blob", "master")
+        case SupportedVCS.Bitbucket | SupportedVCS.BitbucketServer => List("master")
+      }
+
+      maybeSegments.toList.flatMap { segments =>
+        val base = segments.foldLeft(repoUrl)(_ / _)
+        fileNames.map(name => base / name)
+      }
+    }
+
+    val customChangelog = files(possibleChangelogFilenames).map(ReleaseRelatedUrl.CustomChangelog)
+    val customReleaseNotes =
+      files(possibleReleaseNotesFilenames).map(ReleaseRelatedUrl.CustomReleaseNotes)
+
+    github ++ customReleaseNotes ++ customChangelog ++ possibleCompareUrls(
+      vcsType,
+      vcsUri,
+      repoUrl,
+      update
+    )
+  }
+
+  private[vcs] def possibleCompareUrls(
+      vcsType: SupportedVCS,
+      vcsUri: Uri,
+      repoUrl: Uri,
+      update: Update
+  ): List[VersionDiff] = {
+    val from = update.currentVersion
+    val to = update.nextVersion
+
+    extractRepoVCSType(vcsType, vcsUri, repoUrl)
+      .map {
+        case GitHub | Gitlab =>
+          possibleTags(from).zip(possibleTags(to)).map { case (from1, to1) =>
+            VersionDiff(repoUrl / "compare" / s"$from1...$to1")
+          }
+        case Bitbucket | BitbucketServer =>
+          possibleTags(from).zip(possibleTags(to)).map { case (from1, to1) =>
+            VersionDiff((repoUrl / "compare" / s"$to1..$from1").withFragment("diff"))
+          }
+      }
+      .getOrElse(List.empty)
+  }
+
+  private def extractRepoVCSType(
+      vcsType: SupportedVCS,
+      vcsUri: Uri,
+      repoUrl: Uri
+  ): Option[SupportedVCS] = {
+    val host = repoUrl.host.map(_.value)
+    if (vcsUri.host.map(_.value).contains(host.getOrElse("")))
+      Option(vcsType)
+    else
+      host
+        .collect {
+          case "github.com" => GitHub
+          case "gitlab.com" => Gitlab
+        }
+        .orElse {
+          if (host.contains_("bitbucket.org"))
+            Some(Bitbucket)
+          else None
+        }
+  }
+
+  private[vcs] val possibleChangelogFilenames: List[String] = {
+    val baseNames = List(
+      "CHANGELOG",
+      "Changelog",
+      "changelog",
+      "CHANGES"
+    )
+    possibleFilenames(baseNames)
+  }
+
+  private[vcs] val possibleReleaseNotesFilenames: List[String] = {
+    val baseNames = List(
+      "ReleaseNotes",
+      "RELEASES",
+      "Releases",
+      "releases"
+    )
+    possibleFilenames(baseNames)
+  }
+
+  private[vcs] def possibleFilenames(baseNames: List[String]): List[String] = {
+    val extensions = List("md", "markdown", "rst")
+    (baseNames, extensions).mapN { case (base, ext) => s"$base.$ext" }
+  }
+
+  private def possibleTags(version: String): List[String] =
+    List(s"v$version", version, s"release-$version")
 }
--- modules/core/src/test/scala/org/scalasteward/core/vcs/VCSExtraAlgTest.scala
+++ modules/core/src/test/scala/org/scalasteward/core/vcs/VCSExtraAlgTest.scala
@@ -26,14 +26,16 @@ class VCSExtraAlgTest extends AnyFunSuite with Matchers {
   implicit val client = Client.fromHttpApp[IO](routes.orNotFound)
   implicit val httpExistenceClient =
     HttpExistenceClient.create[IO].allocated.map(_._1).unsafeRunSync()
+  val vcsExtraAlg = VCSExtraAlg.create[IO]
 
   val updateFoo = Update.Single("com.example" % "foo" % "0.1.0", Nel.of("0.2.0"))
   val updateBar = Update.Single("com.example" % "bar" % "0.1.0", Nel.of("0.2.0"))
   val updateBuz = Update.Single("com.example" % "buz" % "0.1.0", Nel.of("0.2.0"))
 
-  test("getBranchCompareUrl: std vsc") {
-    val vcsExtraAlg = VCSExtraAlg.create[IO]
+  val update: Update.Single =
+    Update.Single("ch.qos.logback" % "logback-classic" % "1.2.0", Nel.of("1.2.3"))
 
+  test("getBranchCompareUrl: std vsc") {
     vcsExtraAlg
       .getReleaseRelatedUrls(uri"https://github.com/foo/foo", updateFoo)
       .unsafeRunSync() shouldBe List.empty
@@ -69,5 +71,200 @@ class VCSExtraAlgTest extends AnyFunSuite with Matchers {
     githubOnPremVcsExtraAlg
       .getReleaseRelatedUrls(uri"https://github.on-prem.com/foo/buz", updateFoo)
       .unsafeRunSync() shouldBe List.empty
+  }
+
+  test("possibleCompareUrls") {
+    val onPremVCS = "https://github.onprem.io/"
+    val onPremVCSUri = uri"https://github.onprem.io/"
+
+    VCSExtraAlg
+      .possibleCompareUrls(
+        SupportedVCS.GitHub,
+        onPremVCSUri,
+        uri"https://github.com/foo/bar",
+        update
+      )
+      .map(_.url.renderString) shouldBe List(
+      "https://github.com/foo/bar/compare/v1.2.0...v1.2.3",
+      "https://github.com/foo/bar/compare/1.2.0...1.2.3",
+      "https://github.com/foo/bar/compare/release-1.2.0...release-1.2.3"
+    )
+    // should canonicalize (drop last slash)
+    VCSExtraAlg
+      .possibleCompareUrls(
+        SupportedVCS.GitHub,
+        onPremVCSUri,
+        uri"https://github.com/foo/bar/",
+        update
+      )
+      .map(_.url.renderString) shouldBe List(
+      "https://github.com/foo/bar/compare/v1.2.0...v1.2.3",
+      "https://github.com/foo/bar/compare/1.2.0...1.2.3",
+      "https://github.com/foo/bar/compare/release-1.2.0...release-1.2.3"
+    )
+
+    VCSExtraAlg
+      .possibleCompareUrls(
+        SupportedVCS.GitHub,
+        onPremVCSUri,
+        uri"https://gitlab.com/foo/bar",
+        update
+      )
+      .map(_.url.renderString) shouldBe List(
+      "https://gitlab.com/foo/bar/compare/v1.2.0...v1.2.3",
+      "https://gitlab.com/foo/bar/compare/1.2.0...1.2.3",
+      "https://gitlab.com/foo/bar/compare/release-1.2.0...release-1.2.3"
+    )
+    VCSExtraAlg
+      .possibleCompareUrls(
+        SupportedVCS.GitHub,
+        onPremVCSUri,
+        uri"https://bitbucket.org/foo/bar",
+        update
+      )
+      .map(_.url.renderString) shouldBe List(
+      "https://bitbucket.org/foo/bar/compare/v1.2.3..v1.2.0#diff",
+      "https://bitbucket.org/foo/bar/compare/1.2.3..1.2.0#diff",
+      "https://bitbucket.org/foo/bar/compare/release-1.2.3..release-1.2.0#diff"
+    )
+
+    VCSExtraAlg.possibleCompareUrls(
+      SupportedVCS.GitHub,
+      onPremVCSUri,
+      uri"https://scalacenter.github.io/scalafix/",
+      update
+    ) shouldBe List.empty
+
+    VCSExtraAlg
+      .possibleCompareUrls(
+        SupportedVCS.GitHub,
+        onPremVCSUri,
+        onPremVCSUri.addPath("/foo/bar"),
+        update
+      )
+      .map(_.url.renderString) shouldBe List(
+      s"${onPremVCS}foo/bar/compare/v1.2.0...v1.2.3",
+      s"${onPremVCS}foo/bar/compare/1.2.0...1.2.3",
+      s"${onPremVCS}foo/bar/compare/release-1.2.0...release-1.2.3"
+    )
+  }
+
+  test("possibleChangelogUrls: github.com") {
+    VCSExtraAlg
+      .possibleReleaseRelatedUrls(
+        SupportedVCS.GitHub,
+        uri"https://github.com",
+        uri"https://github.com/foo/bar",
+        update
+      )
+      .map(_.url.renderString) shouldBe List(
+      "https://github.com/foo/bar/releases/tag/v1.2.3",
+      "https://github.com/foo/bar/releases/tag/1.2.3",
+      "https://github.com/foo/bar/releases/tag/release-1.2.3",
+      "https://github.com/foo/bar/blob/master/ReleaseNotes.md",
+      "https://github.com/foo/bar/blob/master/ReleaseNotes.markdown",
+      "https://github.com/foo/bar/blob/master/ReleaseNotes.rst",
+      "https://github.com/foo/bar/blob/master/RELEASES.md",
+      "https://github.com/foo/bar/blob/master/RELEASES.markdown",
+      "https://github.com/foo/bar/blob/master/RELEASES.rst",
+      "https://github.com/foo/bar/blob/master/Releases.md",
+      "https://github.com/foo/bar/blob/master/Releases.markdown",
+      "https://github.com/foo/bar/blob/master/Releases.rst",
+      "https://github.com/foo/bar/blob/master/releases.md",
+      "https://github.com/foo/bar/blob/master/releases.markdown",
+      "https://github.com/foo/bar/blob/master/releases.rst",
+      "https://github.com/foo/bar/blob/master/CHANGELOG.md",
+      "https://github.com/foo/bar/blob/master/CHANGELOG.markdown",
+      "https://github.com/foo/bar/blob/master/CHANGELOG.rst",
+      "https://github.com/foo/bar/blob/master/Changelog.md",
+      "https://github.com/foo/bar/blob/master/Changelog.markdown",
+      "https://github.com/foo/bar/blob/master/Changelog.rst",
+      "https://github.com/foo/bar/blob/master/changelog.md",
+      "https://github.com/foo/bar/blob/master/changelog.markdown",
+      "https://github.com/foo/bar/blob/master/changelog.rst",
+      "https://github.com/foo/bar/blob/master/CHANGES.md",
+      "https://github.com/foo/bar/blob/master/CHANGES.markdown",
+      "https://github.com/foo/bar/blob/master/CHANGES.rst",
+      "https://github.com/foo/bar/compare/v1.2.0...v1.2.3",
+      "https://github.com/foo/bar/compare/1.2.0...1.2.3",
+      "https://github.com/foo/bar/compare/release-1.2.0...release-1.2.3"
+    )
+  }
+
+  test("possibleChangelogUrls: gitlab.com") {
+    VCSExtraAlg
+      .possibleReleaseRelatedUrls(
+        SupportedVCS.GitHub,
+        uri"https://github.com",
+        uri"https://gitlab.com/foo/bar",
+        update
+      )
+      .map(_.url.renderString) shouldBe
+      VCSExtraAlg.possibleReleaseNotesFilenames.map(name =>
+        s"https://gitlab.com/foo/bar/blob/master/$name"
+      ) ++
+      VCSExtraAlg.possibleChangelogFilenames.map(name =>
+        s"https://gitlab.com/foo/bar/blob/master/$name"
+      ) ++
+      List(
+        "https://gitlab.com/foo/bar/compare/v1.2.0...v1.2.3",
+        "https://gitlab.com/foo/bar/compare/1.2.0...1.2.3",
+        "https://gitlab.com/foo/bar/compare/release-1.2.0...release-1.2.3"
+      )
+  }
+
+  test("possibleChangelogUrls: on-prem gitlab") {
+    VCSExtraAlg
+      .possibleReleaseRelatedUrls(
+        SupportedVCS.Gitlab,
+        uri"https://gitlab.on-prem.net",
+        uri"https://gitlab.on-prem.net/foo/bar",
+        update
+      )
+      .map(_.url.renderString) shouldBe
+      VCSExtraAlg.possibleReleaseNotesFilenames.map(name =>
+        s"https://gitlab.on-prem.net/foo/bar/blob/master/$name"
+      ) ++
+      VCSExtraAlg.possibleChangelogFilenames.map(name =>
+        s"https://gitlab.on-prem.net/foo/bar/blob/master/$name"
+      ) ++
+      List(
+        "https://gitlab.on-prem.net/foo/bar/compare/v1.2.0...v1.2.3",
+        "https://gitlab.on-prem.net/foo/bar/compare/1.2.0...1.2.3",
+        "https://gitlab.on-prem.net/foo/bar/compare/release-1.2.0...release-1.2.3"
+      )
+  }
+
+  test("possibleChangelogUrls: bitbucket.org") {
+    VCSExtraAlg
+      .possibleReleaseRelatedUrls(
+        SupportedVCS.GitHub,
+        uri"https://github.com",
+        uri"https://bitbucket.org/foo/bar",
+        update
+      )
+      .map(_.url.renderString) shouldBe
+      VCSExtraAlg.possibleReleaseNotesFilenames.map(name =>
+        s"https://bitbucket.org/foo/bar/master/$name"
+      ) ++
+      VCSExtraAlg.possibleChangelogFilenames.map(name =>
+        s"https://bitbucket.org/foo/bar/master/$name"
+      ) ++
+      List(
+        "https://bitbucket.org/foo/bar/compare/v1.2.3..v1.2.0#diff",
+        "https://bitbucket.org/foo/bar/compare/1.2.3..1.2.0#diff",
+        "https://bitbucket.org/foo/bar/compare/release-1.2.3..release-1.2.0#diff"
+      )
+  }
+
+  test("possibleChangelogUrls: homepage") {
+    VCSExtraAlg
+      .possibleReleaseRelatedUrls(
+        SupportedVCS.GitHub,
+        uri"https://github.com",
+        uri"https://scalacenter.github.io/scalafix/",
+        update
+      )
+      .map(_.url.renderString) shouldBe List.empty
   }
 }
